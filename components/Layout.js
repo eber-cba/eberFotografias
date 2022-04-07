07fedecab6b437563b7f1bcc40070bbbfee39b5d
eber-cba <ebercoronel29@gmail.com>
2022-04-07 02:03:41 -0300

    first commit

---
+++ components/Layout.js
@@ -0,0 +1,27 @@
+import Head from "next/head";
+import "bootstrap/dist/css/bootstrap.css";
+import Navbar from "./navbar/Navbar";
+import Script from "next/script";
+export default function Layout({ children }) {
+  return (
+    <div>
+      <Head>
+        <title>Eber Fotografias</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
+          rel="stylesheet"
+          integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
+          crossorigin="anonymous"
+        />
+      </Head>
+      <Navbar />
+      <Script
+        src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+        crossorigin="anonymous"
+      ></Script>
+      <main>{children}</main>
+    </div>
+  );
+}
---
+++ components/navbar/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Carrucel from "./Carrucel";
+
+export default function Navbar() {
+  return (
+    <div className="padreNavbar">
+      <div className="conteinerNavbar">
+        <div>
+          <Carrucel />
+       
+          <label>hola</label>
+         
+        </div>
+      
+      </div>
+    </div>
+  );
+}
